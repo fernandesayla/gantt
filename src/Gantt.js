27224300d7bcf9f1002def959a01babca9a9fb87
Henrique Fidellis <fidellis@bb.com.br>
2017-09-11 13:28:22 -0300

    alt

--- src/Gantt.js
+++ src/Gantt.js
@@ -1,5 +1,5 @@
 
-/* global moment, Snap */
+/* global Snap */
 /**
  * Gantt:
  * 	element: querySelector string, required
@@ -11,6 +11,7 @@ import './gantt.scss';
 
 import Bar from './Bar';
 import Arrow from './Arrow';
+import moment from 'moment';
 
 export default function Gantt(element, projects, config) {
 
