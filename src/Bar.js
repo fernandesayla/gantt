ed3cc67a7237ff85f3c3a6cc547d0c0bf242799d
Henrique Fidellis <fidellis@bb.com.br>
2018-03-28 09:57:28 -0300

    alt

--- src/Bar.js
+++ src/Bar.js
@@ -143,7 +143,7 @@ export default function Bar(gt, task) {
 	function bind() {
 		if (self.invalid) return;
 		setup_click_event();
-		show_details();		
+		show_details();
 		if(gt.config.edit_mode && !task.isGroup && task.progress === 0) {
 			bind_resize();
 			bind_drag();
